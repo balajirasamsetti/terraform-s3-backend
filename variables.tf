51286066396b3675b202e3273867eb2fe2d1ec59
balajirasamsetti <71433872+balajirasamsetti@users.noreply.github.com>
2025-10-23 14:54:53 +0530

    Add Terraform variables for AWS deployment

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "aws_region" {
+  description = "AWS region for deployment"
+  type        = string
+}
+
+variable "state_bucket_name" {
+  description = "S3 bucket for Terraform state"
+  type        = string
+}
+
+variable "lock_table_name" {
+  description = "DynamoDB table for locking"
+  type        = string
+}
+
+variable "state_key" {
+  description = "S3 key path for Terraform state file"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_pair_name" {
+  description = "Name of the existing EC2 key pair"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name tag for EC2 instance"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name (dev/test/prod)"
+  type        = string
+}
