94a1783127c96bb3a02216eb5432621a950e7a79
balajirasamsetti <71433872+balajirasamsetti@users.noreply.github.com>
2025-10-23 15:27:18 +0530

    Add subnet_id to aws_instance resource

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  subnet_id     = var.subnet_id    
   key_name      = var.key_pair_name
 
   tags = {
