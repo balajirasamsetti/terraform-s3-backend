d971da05011152369d4657458b2edce124429563
balajirasamsetti <71433872+balajirasamsetti@users.noreply.github.com>
2025-10-23 15:28:32 +0530

    Add subnet_id variable for EC2 launch configuration

--- variables.tf
+++ variables.tf
@@ -23,6 +23,11 @@ variable "ami_id" {
   type        = string
 }
 
+variable "subnet_id" {
+  description = "Subnet ID where EC2 will be launched"
+  type        = string
+}
+
 variable "instance_type" {
   description = "Type of EC2 instance"
   type        = string
